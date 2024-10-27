13c3852d56c69a9c678c8ba3439920a6971f7f39
RhodeionRX <roman.03.banane20@gmail.com>
2024-10-27 11:22:01 +0000

    feat: configure the catalogue service

--- catalogue-service/src/main.ts
+++ catalogue-service/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService)
+
+  const BROKER_HOST = configService.get<string>('BROKER_HOST');
+  const BROKER_PORT = configService.get<string>('BROKER_PORT');
+  
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        `${BROKER_HOST}:${BROKER_PORT}`,
+      ],
+      queue: 'user_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+
+  await microservice.listen();
 }
+
 bootstrap();
