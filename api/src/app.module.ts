1e448a82d60c8c98a80264be1bf04635ea597d0c
RhodeionRX <roman.03.banane20@gmail.com>
2024-10-30 18:17:24 +0000

    feat: add secret key to crypt an auth token

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ValidationExceptionFilter } from './filters/validation-exception.filter';
 import { BookModule } from './book/book.module';
+import { AuthMiddleware } from './auth/auth.middleware';
+import { BookController } from './book/book.controller';
 
 @Module({
   imports: [
@@ -21,4 +28,11 @@ import { BookModule } from './book/book.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: '*/book/*', method: RequestMethod.GET })
+      .forRoutes(BookController);
+  }
+}
