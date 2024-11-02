e7626e7dc171b924f1f61b9f1ab215bf04e8b8ac
RhodeionRX <roman.03.banane20@gmail.com>
2024-11-02 19:59:26 +0000

    feat: create user decorator

--- api/src/book/book.controller.ts
+++ api/src/book/book.controller.ts
@@ -17,6 +17,8 @@ import { InitBookRequest } from './models/requests/init-book.request';
 import { UpdateBookDto } from './models/update-book.dto';
 import { ClientProxy } from '@nestjs/microservices';
 import { catchError, Observable, throwError } from 'rxjs';
+import { User } from 'src/user/user.decorator';
+import { IUser } from 'src/user/entities/user.entity';
 
 @Controller('book')
 export class BookController {
@@ -32,21 +34,24 @@ export class BookController {
   @Post('/init')
   public async init(
     @Body() initBookRequest: InitBookRequest,
+    @User() user: IUser,
   ): Promise<Observable<any>> {
-    return this.catalogueServiceClient.send('init', initBookRequest).pipe(
-      catchError((error) => {
-        return throwError(
-          () =>
-            new HttpException(
-              {
-                status: HttpStatus.BAD_REQUEST,
-                message: error.message || 'Book creation error',
-              },
-              HttpStatus.BAD_REQUEST,
-            ),
-        );
-      }),
-    );
+    return this.catalogueServiceClient
+      .send('init', { ...initBookRequest, userId: user.id })
+      .pipe(
+        catchError((error) => {
+          return throwError(
+            () =>
+              new HttpException(
+                {
+                  status: HttpStatus.BAD_REQUEST,
+                  message: error.message || 'Book creation error',
+                },
+                HttpStatus.BAD_REQUEST,
+              ),
+          );
+        }),
+      );
   }
 
   @Get()
